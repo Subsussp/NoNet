c08a7924c2015cc950433d79ad570bd01b4ea0c2
Subsussp <130338968+Subsussp@users.noreply.github.com>
2025-08-10 15:54:58 +0300

    Update backend

--- server/src/routes handler/cart.js
+++ server/src/routes handler/cart.js
@@ -30,7 +30,8 @@ Router.route('/add-to-cart').get(isAuth,async function (req,res,next) {
 })
 async function gets(id,inder) {
     let obj = await getiteml(id)
-    return {data: obj ,nun:inder}  
+    let {catg,desc,name,price,_id,material} = obj
+    return {data:{ catg, desc, name, price, _id ,material},nun:inder}  
 }
 Router.route('/').get(isAuth ,async function (req,res,next) {
     let user = await users.findOne({_id:req.session.user._id})
